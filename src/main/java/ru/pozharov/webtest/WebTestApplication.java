0b6643a02d99e36df1d8a0730ea429327c43acee
vikquicker <vik3_3stavrik3@mail.ru>
2021-09-05 15:03:28 +0300

    Initial commit

---
+++ src/main/java/ru/pozharov/webtest/WebTestApplication.java
@@ -0,0 +1,11 @@
+package ru.pozharov.webtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebTestApplication.class, args);
+    }
+}
